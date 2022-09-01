cd53a83e2a02df6233b6be40892450db11b8f897
jmsgnnr <jmsgnnr@gmail.com>
2022-08-31 19:06:08 -0600

    removed repetition, implemented filter on dataframe columns, added pseudo

--- pandas_test.py
+++ pandas_test.py
@@ -1,39 +1,23 @@
-from cgitb import text
-from hashlib import new
 import json
-from this import d
-from typing import final
-from pandas.io.json import json_normalize
 import pandas as pd
-
-# with open("message_1.json", 'r', encoding='utf-8') as json_data:
-#     content = json_data.read()
-#     clean = content.replace(r"[\"\'`'\;\",]`", '')
-#     data = json.loads(clean)
-
 
 with open('message_1.json', 'r', encoding='UTF-8') as json_file:
     data = json.load(json_file)
-# print(data)
-
 
 df1 = pd.DataFrame(data['messages'])
-# print(df1)
-df2 = pd.DataFrame.drop(df1, axis=1,columns=['sender_id_INTERNAL','is_unsent','is_taken_down','bumped_message_metadata','call_duration', 'videos', 'type', 'photos', 'share', 'reactions'])
-# print(df2)
-df3 = df2.dropna(subset= ['content'])
-# print(df3)
-
+# we will add more columns here as we flatten json data
+df1 = df1.filter(['sender_name','timestamp_ms','content'])
+# drop blank content vals
+df1 = df1.dropna(subset=['content'])
 # only returns chat messages, drops 'liked a message'
-df4 = df3[df3["content"].str.contains("Liked a message") == False]
-df4['timestamp_ms'] = pd.to_datetime(df4['timestamp_ms'], unit='ms')
-# print(df4)
-print(df4)
+df1 = df1[df1["content"].str.contains("Liked a message") == False]
+# user may need to change unit type, test data is in Mountain Standard Time
+df1['timestamp_ms'] = pd.to_datetime(df1['timestamp_ms'], unit='ms')
 
 #html output needs latin-1 to parse extra chars in ig download
-result_df = df4.to_html()
+df1_html = df1.to_html()
 text_file = open("html_test.html" , "w", encoding='LATIN-1')
-text_file.write(result_df)
+text_file.write(df1_html)
 text_file.close()
 
 
