2b222ca0a96555e38e473753978388301cd0a2d0
jmsgnnr <jmsgnnr@gmail.com>
2022-09-08 14:35:48 -0600

    added logic for nested components, next step is figure out reassignment using df.setvalue

--- pandas_test.py
+++ pandas_test.py
@@ -1,28 +1,50 @@
 import json
+from lib2to3.pgen2.pgen import DFAState
 import pandas as pd
 
-with open('message_1.json', 'r', encoding='UTF-8') as json_file:
+with open('message_11.json', 'r', encoding='UTF-8') as json_file:
     data = json.load(json_file)
 
-try:
-    df1 = pd.DataFrame(data['messages'])
-    # we will add more columns here as we flatten json data
-    df1 = df1.filter(['sender_name','timestamp_ms','content'])
-    # drop blank content vals
-    df1 = df1.dropna(subset=['content'])
-    # only returns chat messages, drops 'liked a message'
-    df1 = df1[df1["content"].str.contains("Liked a message") == False]
-    # user may need to change unit type, test data is in Mountain Standard Time
-    df1['timestamp_ms'] = pd.to_datetime(df1['timestamp_ms'], unit='ms')
-    #html output needs latin-1 to parse extra chars in ig download
-    df1_html = df1.to_html()
-    with open("html_output.html" , "w", encoding='LATIN-1') as f:
-        f.write(df1_html)
-except:
-    print('this isnt working right, pal')
-    
-f.close()
+test_data = pd.DataFrame(data['messages'])  
 
+try:
+    chat_df = pd.DataFrame(data['messages'])
+    # we will add more columns here as we flatten json data
+
+    chat_df = chat_df.filter(['sender_name','timestamp_ms','content','reactions','share'])
+
+    # grab nested share/reactions 
+    # chat_df['reaction'] =  pd.Series()
+    # for index, row in chat_df.iterrows():
+        
+    #     if type(row['reactions']) == type(list()):
+    #         reaction = (row['reactions'][0]['reaction'])
+    #         print(type(reaction))
+    #         row['reaction'] = reaction
+         # if 'share' in chat_df:
+    #     chat_df['share_text'] = chat_df['share']['share_text']
+    #     chat_df['link'] = chat_df['share']['link']
+    
+    # drops non chat messages
+    # chat_df = chat_df.dropna(subset=['content'])
+
+    # only returns chat messages, drops 'liked a message'
+    # chat_df = chat_df[chat_df["content"].str.contains("Liked a message") == False]
+
+    # user may need to change unit type, test data is in Mountain Standard Time
+
+    chat_df['timestamp_ms'] = pd.to_datetime(chat_df['timestamp_ms'], unit='ms')
+
+    # print(chat_df.dropna(subset=['reaction']))
+
+    #html output needs latin-1 to parse extra chars in ig download
+    chat_df_html = chat_df.to_html()
+
+    with open("html_output.html" , "w", encoding='LATIN-1') as f:
+        f.write(chat_df_html)
+except Exception as e:
+    print(e)
+ 
 
 
 
--- tests/flatten_test.py
+++ tests/flatten_test.py
@@ -1,22 +1,24 @@
 import json
-# import pandas as pd
+import pandas as pd
 
-with open('message_1.json', 'r', encoding='UTF-8') as json_file:
+with open('../message_11.json', 'r', encoding='UTF-8') as json_file:
     data = json.load(json_file)
 
 
-def func1(data):
+def parse_data(data):
+    #  any time we have a function that calls itself, we need a base case! 
+    if data == None: 
+        return
     for key,value in data.items():
         print (str(key)+'->'+str(value))
         if type(value) == type(dict()):
-            func1(value)
+            parse_data(value)
         elif type(value) == type(list()):
             for val in value:
-                if type(val) == type(str()):
-                    pass
-                elif type(val) == type(list()):
-                    pass
-                else:
-                    func1(val)
+                if type(val) != type(str()) or type(val) != type(list()):
+                    parse_data(val)
                     
-new_date = func1(data)
+
+
+
+
