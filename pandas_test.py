a9b65042b6a2d2453e6b767dfd43be5f31de577c
jmsgnnr <jmsgnnr@gmail.com>
2022-09-11 03:02:18 -0600

    testing lambda for reassigning nested dictionary values as reactions column.

--- pandas_test.py
+++ pandas_test.py
@@ -10,17 +10,17 @@ try:
     chat_df = pd.DataFrame(data['messages'])
     # we will add more columns here as we flatten json data
 
-    chat_df = chat_df.filter(['sender_name','timestamp_ms','content','reactions','share'])
+    chat_df = chat_df.filter(['sender_name','timestamp_ms','content'])
 
-    # grab nested share/reactions 
-    # chat_df['reactions_1'].apply(lambda x: [d['reaction'] for d in x])
-    # chat_df['reactions_1'] = chat_df['reactions'].apply(lambda x:  ','.join([i['reaction'] for i in eval(x)]))
+    
+    # grab nested share/reactions
+    # chat_df['react'] = chat_df['reactions'].fillna('[]').apply(lambda x: [d['reaction'] for d in x])
 
     # drops non chat messages
-    # chat_df = chat_df.dropna(subset=['content'])
+    chat_df = chat_df.dropna(subset=['content'])
 
     # only returns chat messages, drops 'liked a message'
-    # chat_df = chat_df[chat_df["content"].str.contains("Liked a message") == False]
+    chat_df = chat_df[chat_df["content"].str.contains("Liked a message") == False]
 
     # user may need to change unit type, test data is in Mountain Standard Time
     chat_df['timestamp_ms'] = pd.to_datetime(chat_df['timestamp_ms'], unit='ms')
@@ -34,7 +34,7 @@ except Exception as e:
     print(e)
 
 
-# print(chat_df)
+print(chat_df)
 
 
 
